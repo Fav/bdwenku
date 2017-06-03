afe83f8dba9476ea29aff17644655fb067d7ab0b
elusive <elusive@elusivedeMacBook-Pro.local>
2017-06-03 11:43:13 +0800

    删除多与代码

--- 百度文库界面清理.user.js
+++ 百度文库界面清理.user.js
@@ -2,26 +2,22 @@
 // @name         百度文库界面清理
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  百度文库界面清理
 // @author       You
 // @match        http://wenku.baidu.com/*
 // @match        https://wenku.baidu.com/*
 // @grant        none
-// @grant        GM_log
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var content = document.getElementById( 'reader-container-inner-1');
-    //console.log(document.body);
+    var content = document.getElementById('reader-container-inner-1');
 
-    //document.body.innerHTML=content.innerHTML;
     var para = document.createElement("div");
     para.innerHTML = '<div style="position:fixed;left:10px;top:50px;border:solid 2px red;padding:10px;z-index:999;" id="mmPrint">清理界面</div>';
     document.body.appendChild(para);
 
-    document.getElementById("mmPrint").onclick=function(){
-        //alert("print");
+    document.getElementById("mmPrint").onclick = function() {
         //删除兄弟节点,删除父节点的兄弟节点
         removeBrother(content);
         //点击全屏按钮
@@ -31,17 +27,14 @@
     };
 
     function removeBrother(elm) {
-        if(elm === null || elm.parentNode === null || elm === document.body){
+        if (elm === null || elm.parentNode === null || elm === document.body) {
             return;
         }
         var p = elm.parentNode.children;
-        for(var i =0,pl= p.length;i<pl;i++) {
-            if(p[i] !== elm && p[i].tagName.toLowerCase() !== "script")
-                p[i].innerHTML="";
+        for (var i = 0, pl = p.length; i < pl; i++) {
+            if (p[i] !== elm && p[i].tagName.toLowerCase() !== "script")
+                p[i].innerHTML = "";
         }
         removeBrother(elm.parentNode);
-
-
     }
-    // Your code here...
 })();
